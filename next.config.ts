993bb299420c4dadb6dc46f96a87c4c3bd8780be
TungUyy <tunguykim@gmail.com>
2025-05-09 19:36:58 +0700

    commit 4

--- next.config.ts
+++ next.config.ts
@@ -8,13 +8,14 @@ const nextConfig = {
     // Tương tự, bỏ qua lỗi TypeScript khi build để tăng tốc
     ignoreBuildErrors: true,
   },
-  swcMinify: true, // Sử dụng SWC để tối ưu hóa
   poweredByHeader: false, // Tắt header X-Powered-By
   reactStrictMode: true,
   images: {
-    formats: ['image/avif', 'image/webp'], 
+    formats: ['image/avif', 'image/webp'],
     // Thêm các domain nếu bạn tải hình ảnh từ bên ngoài
   },
+  // Di chuyển output ra ngoài - đây là cấu hình ở cấp cao nhất
+  output: 'standalone',
 };
 
 export default nextConfig;
