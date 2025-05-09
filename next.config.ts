408bcbf8680559afeebb2ecde08e6bfecbc8f075
TungUyy <tunguykim@gmail.com>
2025-05-09 20:12:33 +0700

    commit 5

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,9 @@ const nextConfig = {
   poweredByHeader: false, // Tắt header X-Powered-By
   reactStrictMode: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/avif', 'image/webp'], 
     // Thêm các domain nếu bạn tải hình ảnh từ bên ngoài
   },
-  // Di chuyển output ra ngoài - đây là cấu hình ở cấp cao nhất
-  output: 'standalone',
 };
 
 export default nextConfig;
